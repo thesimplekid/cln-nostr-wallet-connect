e379512b08d567a83bcbbff5668eb306bfe6ecca
thesimplekid <tsk@thesimplekid.com>
2024-07-12 09:56:24 +0100

    feat: flakebox

--- flake.nix
+++ flake.nix
@@ -1,21 +1,60 @@
 {
+  description = "CLN nostr wallet connect";
+
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    crane.url = "github:ipetkov/crane";
-    crane.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+
+    flakebox = {
+      url = "github:rustshop/flakebox";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, crane, flake-utils, ... }:
+  outputs = { self, nixpkgs, flakebox, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        craneLib = crane.lib.${system};
-      in
-    {
-      packages.default = craneLib.buildPackage {
-        src = craneLib.cleanCargoSource ./.;
-        buildInputs = [];
-        nativeBuildInputs = [];
-      };
-    });
+        pkgs = import nixpkgs { system = system; };
+        lib = pkgs.lib;
+        flakeboxLib = flakebox.lib.${system} { };
+        rustSrc = flakeboxLib.filterSubPaths {
+          root = builtins.path {
+            name = "cln-nostr-wallet-connect";
+            path = ./.;
+          };
+          paths = [  ];
+        };
+
+        targetsStd = flakeboxLib.mkStdTargets { };
+        toolchainsStd = flakeboxLib.mkStdToolchains { };
+
+        toolchainNative = flakeboxLib.mkFenixToolchain {
+          targets = (pkgs.lib.getAttrs [ "default" ] targetsStd);
+        };
+
+        commonArgs = {
+          buildInputs = [ pkgs.openssl ] ++ lib.optionals pkgs.stdenv.isDarwin
+            [ pkgs.darwin.apple_sdk.frameworks.SystemConfiguration ];
+          nativeBuildInputs = [ pkgs.pkg-config ];
+        };
+        outputs = (flakeboxLib.craneMultiBuild { toolchains = toolchainsStd; })
+          (craneLib':
+            let
+              craneLib = (craneLib'.overrideArgs {
+                pname = "flexbox-multibuild";
+                src = rustSrc;
+              }).overrideArgs commonArgs;
+            in rec {
+              workspaceDeps = craneLib.buildWorkspaceDepsOnly { };
+              workspaceBuild =
+                craneLib.buildWorkspace { cargoArtifacts = workspaceDeps; };
+            });
+      in {
+        devShells = flakeboxLib.mkShells {
+          toolchain = toolchainNative;
+          packages = [ ];
+          nativeBuildInputs = with pkgs; [ wasm-pack sqlx-cli ];
+        };
+      });
 }
